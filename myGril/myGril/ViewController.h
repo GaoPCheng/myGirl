277ee30b07eb6f5f76a74be4e5267ef4c7bbefba
gpc <gpc@qq.com>
2017-05-15 19:37:42 +0800

    初始化.

---
+++ myGril/myGril/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myGril
+//
+//  Created by 高鹏程 on 17/5/15.
+//  Copyright © 2017年 gpc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ myGril/myGril/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  myGril
+//
+//  Created by 高鹏程 on 17/5/15.
+//  Copyright © 2017年 gpc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
